199e861efbf51b3714a359b7d6c20911259262df
paguii <pagui-guilherme@hotmail.com>
2017-07-05 17:32:14 -0300

    Adicionando layout para as futuras views

---
+++ resources/views/iService.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.default')
+
+@section('pageName', 'Teste')
+
+@section('header')
+<h1>Pagina Teste</h1>
+@endsection
+
+@section('content')
+<article class="">
+  <p>Conteudo de Teste</p>
+</article>
+@endsection
+
+@section('footer')
+<h2>Rodapé</h2>
+@endsection
